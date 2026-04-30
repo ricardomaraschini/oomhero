42dfad44a8dd30aaf760c711f395dd83014fda06
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2026-04-30 16:21:16 +0200

    chore: start using traits for objects
    in order to make this code testable we need to leverage traits. this
    commit introduces all traits i deem necessary for the unit tests that
    are to come.
    
    while doing so quite a lot of comments typos were fixed.

--- src/errors.rs
+++ src/errors.rs
@@ -3,7 +3,7 @@ use std::io;
 use std::num;
 use thiserror::Error;
 
-// Error enum encapsulates all errors that this libray can return. with it we can easily just
+// Error enum encapsulates all errors that this library can return. With it we can easily just
 // return a variety of errors without worrying that much about matching and converting.
 #[derive(Error, Debug)]
 pub enum Error {
